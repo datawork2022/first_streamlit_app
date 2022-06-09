7fc5aa38f493beeac9031bc305f07942ed82c95d
Datawork <106117393+datawork2022@users.noreply.github.com>
2022-06-09 16:54:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.header('fruityvice Fruit Advice !')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just write the data to the screen
 
 #take the json verson of the response and normalize it
 fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
